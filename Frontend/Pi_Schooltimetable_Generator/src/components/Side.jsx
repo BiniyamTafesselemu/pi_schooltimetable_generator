5efc00311f5b19709041cb0a5f160587122679bf
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-09-03 00:03:04 +0200

    sideBar logo insert

--- Frontend/Pi_Schooltimetable_Generator/src/components/Side.jsx
+++ Frontend/Pi_Schooltimetable_Generator/src/components/Side.jsx
@@ -1,8 +1,9 @@
-import { Sidebar } from "flowbite-react";
+import logo from '../assets/pi-school-system-logo-only.png'
 
 export default function Side(){
     return(
-        <div className='bg-[#f1f1f1] border-r-[0.1rem]  border-solid border-[rgb(167,115,222)] flex flex-col h-screen  items-start justify-center p-6 gap-3 w-fit text-xl'>
+        <div className='bg-[#f1f1f1] border-r-[0.1rem]  border-solid border-[rgb(167,115,222)] flex flex-col h-screen  items-start justify-start p-6 gap-5 w-fit text-xl'>
+            <img src={logo} className='w-20 mb-24'></img>
             <a href="/Sections" className="hover:text-[#5E469C]">Sections</a>
             <a href="/Subjects" className="hover:text-[#5E469C]">
                         Subjects
