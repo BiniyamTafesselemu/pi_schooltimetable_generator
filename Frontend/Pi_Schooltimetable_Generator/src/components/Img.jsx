062139a03cd084585063f0ac9ff838c368b2f46a
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-09-17 12:39:11 +0200

    clean up

--- Frontend/Pi_Schooltimetable_Generator/src/components/Img.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Img =({className, src, alt = "background image", ...restProps}) => {
-  return (
-    <img className={className} src={src} alt = {alt} {...restProps} loading={"lazy"}/>
-  )
-}
-
-export default Img;
